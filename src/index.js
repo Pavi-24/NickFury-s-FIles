54618852cc3cc64f6d6b7f6f112a4d6d53ce6d62
Paikhomba W <wahengbamkumar546@gmail.com>
2025-04-16 19:58:29 +0530

    Project Setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+//MIDDLEWARES 
+
+app.use(express.json());
+app.use(cors);
+
+//ROUTES
+
+
+//ERROR HANDLING MIDDLEWARES 
+
+
+//SERVER RUNNING
+app.listen(port,()=>{
+    console.log(`Server is running on localhost port ${port}`);
+});
